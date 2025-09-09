4067111df047a4b51b27ab12f00ef02f4ee7238a
Felix Mandyme <111139677+Felix-Red@users.noreply.github.com>
2025-09-09 08:51:46 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/duckproject/MainActivity.kt
+++ app/src/main/java/com/example/duckproject/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,7 +40,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DuckProjectTheme {
-                NavigationRoot()
+                Surface{
+                    NavigationRoot()
+                }
             }
         }
     }
